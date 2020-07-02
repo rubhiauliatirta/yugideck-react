50ed0e74922404faec785111e7b8e216821ecffd
Rubhi Aulia Tirta <rubhiauliatirtacom@gmail.com>
2020-07-02 08:28:26 +0700

    add href source

--- src/views/TechStack.js
+++ src/views/TechStack.js
@@ -24,6 +24,7 @@ export default function TechStack() {
             <p data-aos="fade-down">YugiDeck is an serverless app that allows user to build unlimited deck from <strong>Japanese Yu-Gi-Oh! Trading Card Game</strong> developed by Konami. This app is using YGOPRODeck public Yu-Gi-Oh! API to access enormous Yu-Gi-Oh! card database with over 10.000 card available to view.</p>
             <p data-aos="fade-down">I created this app to have deeper understanding about React, especially React Hooks. I have learned that we can use hooks to separate stateful logic in our components to other function, make our components cleaner and also allows us to reuse that stateful logic. Hooks are not only making our functional components can have state like class component, but it’s more than that!</p>
             <p data-aos="fade-down">In this application, I also focus on studying Firebase services like Authentication, Cloud Function, Cloud Firestore, and Hosting. I have learned to writing security rules and using cloud functions to restrict and validate CRUD operation on our collections in Cloud Firestore.</p>
+            <p data-aos="fade-down" style={{marginTop: "5rem"}}> Source Code on <a href="https://github.com/rubhiauliatirta/yugideck-react">Github</a> </p>
         </TextYugi>
       </div>
       <div style={styles.separator} className="w-100"></div>
